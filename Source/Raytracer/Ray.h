1176fd8d957db78f59ca1e8aab7ca0fb8c7c4eac
yourespeakingtothemoon <91557919+yourespeakingtothemoon@users.noreply.github.com>
2023-11-28 16:50:24 -0700

    everybody loves RAYmond part deux

--- Source/Raytracer/Ray.h
+++ Source/Raytracer/Ray.h
@@ -9,7 +9,12 @@ struct ray_t
 	ray_t()= default;
 	ray_t(const glm::vec3& origin, const glm::vec3& direction) : origin(origin), direction(direction) {}
 
-glm::vec3	GetPoint(float distance) const { return origin + direction * distance; }
+	glm::vec3 GetPoint(float distance) const { return origin + direction * distance; }
+
+	//glm::vec3 operator * (float distance) const { return origin + direction * distance};
+	//alternatively we can just point to my fancy function
+	glm::vec3 operator * (float distance) const { return GetPoint(distance); }
+
 
 	glm::vec3 origin;
 	glm::vec3 direction;
